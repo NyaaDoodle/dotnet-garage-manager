9a7ab3bb7fb5e2423b407943150681db7a980a48
Melanie Sverdlov <nyaadoodle@gmail.com>
2025-01-08 10:51:15 +0200

    Wheel implementation WIP

--- Ex03.GarageLogic/Vehicles/Wheel.cs
+++ Ex03.GarageLogic/Vehicles/Wheel.cs
@@ -5,12 +5,26 @@ namespace Ex03.GarageLogic.Vehicles
     internal class Wheel
     {
         public string ManufacturerName { get; set; }
-        public float? CurrentAirPressureLevel { get; set; }
+
+        public float? CurrentAirPressureLevel { get; private set; } = 0;
         public float? MaximumAllowedAirPressureLevel { get; set; }
 
         public void Inflate(float i_AirPressureToAdd)
         {
-            
+            try
+            {
+                float currentAirPressureLevel = CurrentAirPressureLevel.Value;
+                float maximumAllowedAirPressureLevel = MaximumAllowedAirPressureLevel.Value;
+                float newAirPressureLevel = currentAirPressureLevel + i_AirPressureToAdd;
+                if (newAirPressureLevel <= MaximumAllowedAirPressureLevel)
+                {
+                    CurrentAirPressureLevel = newAirPressureLevel;
+                }
+                else
+                {
+                    
+                }
+            }
         }
     }
 }
